c6274271e6dd6dcee5f01c451183a952cd683737
jacekd <djack@djack.com.pl>
2019-03-19 11:07:51 +0100

    Update device.cpp

--- supla-server/src/device/device.cpp
+++ supla-server/src/device/device.cpp
@@ -390,6 +390,8 @@ void supla_device::on_channel_set_value_result(
 
 bool supla_device::get_channel_value(int ChannelID,
                                      char value[SUPLA_CHANNELVALUE_SIZE]) {
+  supla_log(LOG_DEBUG, "==>get_channel_value: channel:%i", ChannelId);
+  supla_log(LOG_DEBUG, "==>get_channel_value: value:%s", value);
   return channels->get_channel_value(ChannelID, value);
 }
 
